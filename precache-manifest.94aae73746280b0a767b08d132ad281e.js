c04feaf0bab28c801ad70cf92998e9f3a1284e79
mm013 <mmfarhan13@gmail.com>
2020-05-28 14:16:42 +0530

    Updates

---
+++ precache-manifest.94aae73746280b0a767b08d132ad281e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c65594edbbb8a34f54f0cffb1112f5",
+    "url": "/RailYathri-5/index.html"
+  },
+  {
+    "revision": "62d3690db7b6a5ec7e73",
+    "url": "/RailYathri-5/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "381df1560ef67d1c1094",
+    "url": "/RailYathri-5/static/css/main.44cd62c8.chunk.css"
+  },
+  {
+    "revision": "62d3690db7b6a5ec7e73",
+    "url": "/RailYathri-5/static/js/2.cbc672b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RailYathri-5/static/js/2.cbc672b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "381df1560ef67d1c1094",
+    "url": "/RailYathri-5/static/js/main.c85ca117.chunk.js"
+  },
+  {
+    "revision": "b0f40319239a90a72035",
+    "url": "/RailYathri-5/static/js/runtime-main.ba0230ce.js"
+  },
+  {
+    "revision": "04bae48145dfd8c771d9b6e250c573d0",
+    "url": "/RailYathri-5/static/media/Group 173.04bae481.svg"
+  },
+  {
+    "revision": "919dbffaf2c1fa5044a02b6963ae2373",
+    "url": "/RailYathri-5/static/media/Path 563.919dbffa.svg"
+  },
+  {
+    "revision": "8e2f9838a5e7fa193590cf451ba8d917",
+    "url": "/RailYathri-5/static/media/Path 565.8e2f9838.svg"
+  },
+  {
+    "revision": "27c95b9dbe48c6014e7f3cafd21e79fc",
+    "url": "/RailYathri-5/static/media/icon-train-details.27c95b9d.svg"
+  }
+]);
